3ae6a47226c273440b42e621e52406c4a8e5be84
Aaron Parker <aaron@stealthpuppy.com>
2019-03-28 18:02:01 +1100

    Create Optimise.ps1

---
+++ tests/Optimise.ps1
@@ -0,0 +1,86 @@
+# AppVeyor Testing
+If (Test-Path 'env:APPVEYOR_BUILD_FOLDER') {
+    $projectRoot = $env:APPVEYOR_BUILD_FOLDER
+}
+Else {
+    # Local Testing 
+    $projectRoot = ((Get-Item (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)).Parent).FullName
+}
+Write-Host "Project root is: $projectRoot"
+
+#region Functions
+Function Invoke-Process {
+    <# 
+    .DESCRIPTION 
+        Invoke-Process is a simple wrapper function that aims to "PowerShellyify" launching typical external processes. There 
+        are lots of ways to invoke processes in PowerShell with Start-Process, Invoke-Expression, & and others but none account 
+        well for the various streams and exit codes that an external process returns. Also, it's hard to write good tests 
+        when launching external proceses. 
+    
+        This function ensures any errors are sent to the error stream, standard output is sent via the Output stream and any 
+        time the process returns an exit code other than 0, treat it as an error. 
+    #> 
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $FilePath,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string] $ArgumentList
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    try {
+        $stdOutTempFile = "$env:TEMP\$((New-Guid).Guid)"
+        $stdErrTempFile = "$env:TEMP\$((New-Guid).Guid)"
+
+        $startProcessParams = @{
+            FilePath               = $FilePath
+            ArgumentList           = $ArgumentList
+            RedirectStandardError  = $stdErrTempFile
+            RedirectStandardOutput = $stdOutTempFile
+            Wait                   = $true;
+            PassThru               = $true;
+            NoNewWindow            = $true;
+        }
+        if ($PSCmdlet.ShouldProcess("Process [$($FilePath)]", "Run with args: [$($ArgumentList)]")) {
+            $cmd = Start-Process @startProcessParams
+            $cmdOutput = Get-Content -Path $stdOutTempFile -Raw
+            $cmdError = Get-Content -Path $stdErrTempFile -Raw
+            if ($cmd.ExitCode -ne 0) {
+                if ($cmdError) {
+                    throw $cmdError.Trim()
+                }
+                if ($cmdOutput) {
+                    throw $cmdOutput.Trim()
+                }
+            }
+            else {
+                if ([string]::IsNullOrEmpty($cmdOutput) -eq $false) {
+                    Write-Output -InputObject $cmdOutput
+                }
+            }
+        }
+    }
+    catch {
+        $PSCmdlet.ThrowTerminatingError($_)
+    }
+    finally {
+        Remove-Item -Path $stdOutTempFile, $stdErrTempFile -Force -ErrorAction Ignore
+    }
+}
+#endregion
+
+#region Optimise images
+$pngout = "$projectRoot\pngout\pngout.exe"
+$icons = "$projectRoot\icons"
+Push-Location $icons
+$images = Get-ChildItem -Path $icons -Recurse -Include *.*
+ForEach ($image in $images) {
+    $result = Invoke-Process -FilePath $pngout -ArgumentList $image.FullName -Verbose
+}
+Pop-Location
+#endregion
