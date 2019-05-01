251426c83da009b2d94e837852fb81ff55ead291
Aaron Parker <aaron@stealthpuppy.com>
2019-05-01 14:13:39 +1000

    OneNote Metro; Image dimension tests

--- tests/Icon.Tests.ps1
+++ tests/Icon.Tests.ps1
@@ -11,19 +11,25 @@ Else {
 $icons = Join-Path $projectRoot "icons"
 $images = Get-ChildItem -Path $icons -Recurse -Include *.*
 
-Describe "All images should be in PNG format" {
+Describe "Image format tests" {
     ForEach ($image in $images) {
         It "$($image.Name) should be a .PNG file" {
             [IO.Path]::GetExtension($image.Name) -match ".png" | Should -Be $True
         }
     }
 }
-Describe "All images should be square" {
+Describe "Image dimension tests" {
     Add-Type -AssemblyName System.Drawing
     ForEach ($image in $images) {
         $png = New-Object System.Drawing.Bitmap $image.FullName
         It "$($image.Name) should match height and width" {
             $png.Height -eq $png.Width | Should -Be $True
+        }
+        It "$($image.Name) height should be 256 pixels or more" {
+            $png.Height -ge 256 | Should -Be $True
+        }
+        It "$($image.Name) width should be 256 pixels or more" {
+            $png.Width -ge 256 | Should -Be $True
         }
     }
 }
